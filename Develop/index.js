d6e7b26338c0a9d97d5690237b724d0fe2daad70
unknown <nataliematos12@gmail.com>
2023-04-04 23:11:44 -0400

    Added starter code to repo

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,13 @@
-const CLI = require('./lib/cli.js');
+// TODO: Include packages needed for this application
 
-const cli = new CLI();
+// TODO: Create an array of questions for user input
+const questions = [];
 
-cli.run();
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
